4f0056c8603811081853f8669253d319bc627b74
Spencer Mullinix <spencermullinix@gmail.com>
2023-06-25 08:43:53 -0600

    logging

--- lock/MFRC522.py
+++ lock/MFRC522.py
@@ -25,6 +25,8 @@ import RPi.GPIO as GPIO
 import spi
 import signal
 import time
+
+import logging
   
 class MFRC522:
   NRSTPD = 22
@@ -307,7 +309,7 @@ class MFRC522:
     (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, buf)
     
     if (status == self.MI_OK) and (backLen == 0x18):
-      # print "Size: " + str(backData[0])
+      # logging.debug( "Size: " + str(backData[0]))
       return    backData[0]
     else:
       return 0
@@ -338,9 +340,9 @@ class MFRC522:
 
     # Check if an error occurred
     if not(status == self.MI_OK):
-      # print "AUTH ERROR!!"
+      logging.debug("AUTH ERROR!!"))
     if not (self.Read_MFRC522(self.Status2Reg) & 0x08) != 0:
-      # print "AUTH ERROR(status2reg & 0x08) != 0"
+      logging.debug("AUTH ERROR(status2reg & 0x08) != 0")
 
     # Return the status
     return status
@@ -357,10 +359,10 @@ class MFRC522:
     recvData.append(pOut[1])
     (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, recvData)
     if not(status == self.MI_OK):
-      # print "Error while reading!"
+      # logging.debug( "Error while reading!")
     i = 0
     if len(backData) == 16:
-      # print "Sector "+str(blockAddr)+" "+str(backData)
+      # logging.debug( "Sector "+str(blockAddr)+" "+str(backData))
   
   def MFRC522_Write(self, blockAddr, writeData):
     buff = []
@@ -373,7 +375,7 @@ class MFRC522:
     if not(status == self.MI_OK) or not(backLen == 4) or not((backData[0] & 0x0F) == 0x0A):
         status = self.MI_ERR
     
-    # print "%s backdata &0x0F == 0x0A %s" % (backLen, backData[0]&0x0F)
+    # logging.debug( "%s backdata &0x0F == 0x0A %s" % (backLen, backData[0]&0x0F))
     if status == self.MI_OK:
         i = 0
         buf = []
@@ -385,9 +387,9 @@ class MFRC522:
         buf.append(crc[1])
         (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE,buf)
         if not(status == self.MI_OK) or not(backLen == 4) or not((backData[0] & 0x0F) == 0x0A):
-            # print "Error while writing"
+            # logging.debug( "Error while writing")
         if status == self.MI_OK:
-            # print "Data written"
+            # logging.debug( "Data written")
 
   def MFRC522_DumpClassic1K(self, key, uid):
     i = 0
@@ -397,7 +399,7 @@ class MFRC522:
         if status == self.MI_OK:
             self.MFRC522_Read(i)
         else:
-            # print "Authentication error"
+            # logging.debug( "Authentication error")
         i = i+1
 
   def MFRC522_Init(self):
